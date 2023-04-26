57cf4b1bc24fdf5f396c3c21dcdd38884f26a099
Gunjan-Patidar <patidargunjan54@gmail.com>
2023-04-26 09:52:14 +0530

    added one line in varifylogintestcase

--- src/test/java/TestCases/verifylogintestcase.java
+++ src/test/java/TestCases/verifylogintestcase.java
@@ -21,7 +21,9 @@ public class verifylogintestcase extends BaseClass {
 		  loginpageobjectmodel lpo= new loginpageobjectmodel (driver);//for call method we create object/ lpo is shortform of=LoginPageObject  // we gave driver here bcoz to give scope to loginpagwobject usko scope nahi tha
 		  // jaise humne driver dala error aya so we will use constructor 
 		  
-		 
+		 // added one line for demo purpose 
+		  //this line is added for demo 2 
+		  
 		 lpo.EnterUsername().sendKeys(TestCaseDAtaforlogin.username );//method static hai , or static ko call karne ke liye direct cls ka name diya to call hoti hai //thz will take data from TestCaseDataforlogin here we hide hardcoded data
 		  lpo.EnterPassword().sendKeys(TestCaseDAtaforlogin.password);
 		lpo.clicklogin().click();
